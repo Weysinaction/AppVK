e6be68e7b47a688d4b33370d7e049ac1767fdb5e
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-08-05 15:40:41 +0300

    add observers for keyboard and check for empty fields

--- AppVK/AppVK/ViewController.swift
+++ AppVK/AppVK/ViewController.swift
@@ -5,7 +5,76 @@ import UIKit
 
 /// ViewController-
 class ViewController: UIViewController {
+    @IBOutlet var loginScrollView: UIScrollView!
+
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+
+    var scrollViewTapGestureRecognizer = UITapGestureRecognizer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        scrollViewTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        loginScrollView.addGestureRecognizer(scrollViewTapGestureRecognizer)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyBoardWillShown(notification:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyBoardWillHide(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel)
+
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+
+    @objc func keyBoardWillShown(notification: Notification) {
+        let info = notification.userInfo as NSDictionary?
+        let kbSize = (info?.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as? NSValue)?.cgRectValue.size
+
+        let contentInset = UIEdgeInsets(top: 0, left: 0, bottom: kbSize?.height ?? 0, right: 0)
+
+        loginScrollView.contentInset = contentInset
+        loginScrollView.scrollIndicatorInsets = contentInset
+    }
+
+    @objc func keyBoardWillHide(notification: Notification) {
+        loginScrollView.contentInset = UIEdgeInsets.zero
+        loginScrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
+
+    @objc func hideKeyboard() {
+        loginScrollView.endEditing(true)
+    }
+
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        guard let login = loginTextField.text, let password = passwordTextField.text else { return }
+
+        if login.isEmpty, password.isEmpty {
+            showAlert(title: "Ошибка!", message: "Неправильный логин/пароль")
+        }
     }
 }
