4fde38d76fadd0663dcc3ccef0d44ab2027d51ef
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-08-06 01:55:37 +0300

    Добавил таб бар контроллер, навигейшн вью и тэйбл вью контроллеры, настроил переходы между ними и добавил классы для друзей и сообществ.

---
+++ AppVK/AppVK/FriendsTableViewController.swift
@@ -0,0 +1,21 @@
+// FriendsTableViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// FriendsTableViewController-
+final class FriendsTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+}
