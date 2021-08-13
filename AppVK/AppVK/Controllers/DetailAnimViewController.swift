68453a85c202337a95b1056a145e2449bd967b43
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-08-13 16:26:21 +0300

    Добавил вью контроллер для анимации фотографий

---
+++ AppVK/AppVK/Controllers/DetailAnimViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailAnimViewController.swift
+//  AppVK
+//
+//  Created by Владислав Лазарев on 13.08.2021.
+//
+
+import UIKit
+
+/// DetailAnimViewController-
+class DetailAnimViewController: UIViewController {
+
+    //MARK: IBOutlet
+    @IBOutlet private var photoImageView: UIImageView!
+    
+    //MARK: private properties
+    
+    //MARK: DetailAnimViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+    //MARK: private methods
+}
